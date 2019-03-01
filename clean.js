87d5a27c9cd0e8c7d2f0b85841931640395821c8
luke <lukejpreston@gmail.com>
2019-03-01 23:18:20 +0000

    can clean

--- clean.js
+++ clean.js
@@ -13,25 +13,25 @@ const timeWizard = require('./time-wizard')
 const getNoise = async (audio, name, start, end) => sox(`${audio} noise-${name}.wav trim ${start} ${timeWizard.diff(start, end)}`)
 const getNoiseProfile = async (name) => sox(`noise-${name}.wav -n noiseprof ${name}-noise.prof`)
 const removeNoiseAndClean = async (audio, name) => sox(`${audio} ${name}-clean.wav noisered ${name}-noise.prof 0.21 compand 0.1,0.3 -60,-60,-30,-15,-20,-12,-4,-8,-2,-7 -2 norm -3 highpass 10`)
-const trim = async (name, start, end) => sox(`${name}-clean.wav trim ${start} ${end - start}`)
+const trimAudio = async (name, start, end) => sox(`${name}-clean.wav ${name}-clean-clipped.wav trim ${start} ${timeWizard.diff(start, end)}`)
 
 const clean = async () => {
   const episode = 'e041'
   const name = `${episode}-luke`
   const audio = './data/e041/e041-luke.wav'
   const noise = {
-    start: '00:24',
-    end: '00:34'
+    start: 24,
+    end: 34
   }
   const trim = {
-    start: '00:24',
-    end: '40:45'
+    start: 24,
+    end: '40:47'
   }
 
   await getNoise(audio, name, noise.start, noise.end)
-//   await getNoiseProfile(name)
-//   await removeNoiseAndClean(audio, name)
-//   await trim(name, trim.start, trim.end)
+  await getNoiseProfile(name)
+  await removeNoiseAndClean(audio, name)
+  await trimAudio(name, trim.start, trim.end)
 }
 clean()
 
