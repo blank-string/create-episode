b186adb0679dbb9d2725b235d559637592de61f8
luke <lukejpreston@gmail.com>
2019-03-02 07:28:50 +0000

    split out args

---
+++ args.js
@@ -0,0 +1,57 @@
+const fs = require('fs-extra')
+const path = require('path')
+const yargs = require('yargs')
+const chalk = require('chalk')
+
+const args = yargs
+  .option('name', {
+    alias: 'n',
+    describe: 'The name of the episode, also the name of the sub folder'
+  })
+  .option('dir', {
+    alias: 'd',
+    describe: 'the directory where all the data is stored'
+  })
+  .help()
+  .example(`Folder Structure:
+  ./data
+  ├── audio
+  │   ├── closing.mp3
+  │   ├── opening.mp3
+  │   ├── sfx-1.mp3
+  │   └── sfx-2.mp3
+  └── e01
+  .   ├── person-1.json
+  .   ├── person-1.wav
+  .   ├── person-2.json
+  .   ├── person-2.mp3
+  .   ├── person-3.json
+  .   └── person-3.aif
+
+create-episode -d ./data -name e01
+`).argv
+
+const error = (message) => {
+  console.log(chalk.red(message))
+  console.log()
+  yargs.showHelp()
+  process.exit(1)
+}
+
+const name = args.name
+const dir = args.dir
+if (typeof name === 'undefined') error('--name -n is requireed')
+if (typeof dir === 'undefined') error('--dir -d is requireed')
+
+const dataDir = path.resolve(process.cwd(), dir)
+if (!fs.existsSync(dataDir)) error(`${dataDir} does not exist`)
+
+const metaDir = path.join(dataDir, name)
+if (!fs.existsSync(metaDir)) error(`${metaDir} does not exist`)
+
+const meta = fs.readdirSync(metaDir)
+  .map(filename => path.join(metaDir, filename))
+  .filter(filename => filename.includes('.json'))
+  .map(filename => require(filename))
+
+module.exports = meta
--- clean.js
+++ clean.js
@@ -3,17 +3,12 @@ const fs = require('fs-extra')
 const sox = require('./sox')
 const timeWizard = require('./time-wizard')
 
-// sox $in noise-$name.wav trim $noise_start $noise_end
-// sox noise-$name.wav -n noiseprof $name-noise.prof
-// sox $in $out noisered $name-noise.prof 0.21 compand 0.1,0.3 -60,-60,-30,-15,-20,-12,-4,-8,-2,-7 -2 norm -3 highpass 10
+// want this to not just save locally but to a tmp folder, which can then be clean
 
-// sox $name.wav $name-end.wav trim 0 $end
-// sox $name-end.wav $name-clipped.wav trim $clap
+const trimAudio = async (audio, name, start, end) => sox(`${audio} ${name}.wav trim ${start} ${timeWizard.diff(start, end)}`)
 
-const getNoise = async (audio, name, start, end) => sox(`${audio} noise-${name}.wav trim ${start} ${timeWizard.diff(start, end)}`)
 const getNoiseProfile = async (name) => sox(`noise-${name}.wav -n noiseprof ${name}-noise.prof`)
 const removeNoiseAndClean = async (audio, name) => sox(`${audio} ${name}-clean.wav noisered ${name}-noise.prof 0.21 compand 0.1,0.3 -60,-60,-30,-15,-20,-12,-4,-8,-2,-7 -2 norm -3 highpass 10`)
-const trimAudio = async (name, start, end) => sox(`${name}-clean.wav ${name}-clean-clipped.wav trim ${start} ${timeWizard.diff(start, end)}`)
 
 const clean = async () => {
   const episode = 'e041'
@@ -23,15 +18,15 @@ const clean = async () => {
     start: 24,
     end: 34
   }
-  const trim = {
-    start: 24,
-    end: '40:47'
-  }
+  //   const trim = {
+  //     start: 24,
+  //     end: '40:47'
+  //   }
 
-  await getNoise(audio, name, noise.start, noise.end)
+  await trimAudio(audio, `noise-${name}`, noise.start, noise.end)
   await getNoiseProfile(name)
   await removeNoiseAndClean(audio, name)
-  await trimAudio(name, trim.start, trim.end)
+//   await trimAudio(`${name}-clean.wav`, `${name}-clean-clipped`, trim.start, trim.end)
 }
 clean()
 
